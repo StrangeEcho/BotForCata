46a4f80ffe67aacd34e5f3c25b3b30ec7932334e
Cata <ole.kh@hotmail.com>
2020-12-09 03:39:39 +0100

    Moved util and config files out of index.

---
+++ util.js
@@ -0,0 +1,8 @@
+module.exports.clean = (text = new String()) => {
+	if (typeof (text) === "string") {
+		return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+	}
+	else {
+		return text;
+	}
+};
